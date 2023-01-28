f80e930f97527fe27fd6b16240179a1ce410a1da
tambordini <37735373+tambordini@users.noreply.github.com>
2023-01-28 11:45:06 +0700

    Add antd

--- App.tsx
+++ App.tsx
@@ -1,11 +1,23 @@
 import * as React from 'react';
+import 'antd/dist/reset.css';
 import './style.css';
+import { Button, ConfigProvider } from 'antd';
+import { ConfigProviderProps } from 'antd/es/config-provider';
 
 export default function App() {
+  const configProviderProps = React.useMemo((): ConfigProviderProps => {
+    return {
+      theme: {
+        token: {
+          colorPrimary: '#00b96b',
+        },
+      },
+    };
+  }, []);
+
   return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
-    </div>
+    <ConfigProvider {...configProviderProps}>
+      <Button>Hello</Button>
+    </ConfigProvider>
   );
 }
--- index.tsx
+++ index.tsx
@@ -1,3 +1,4 @@
+import { ConfigProvider } from 'antd';
 import * as React from 'react';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
