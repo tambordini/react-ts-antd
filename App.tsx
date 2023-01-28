ab865c1a41d14292f1fa0de344c0bf9feb2d4bce
tambordini <37735373+tambordini@users.noreply.github.com>
2023-01-28 12:00:01 +0700

    Add config color data

--- App.tsx
+++ App.tsx
@@ -3,13 +3,16 @@ import 'antd/dist/reset.css';
 import './style.css';
 import { Button, ConfigProvider } from 'antd';
 import { ConfigProviderProps } from 'antd/es/config-provider';
+import { colorConfig } from './data/color-config';
 
-export default function App() {
+export default function App(): React.ReactElement {
+  const { colorPrimary } = colorConfig;
+
   const configProviderProps = React.useMemo((): ConfigProviderProps => {
     return {
       theme: {
         token: {
-          colorPrimary: '#00b96b',
+          colorPrimary,
         },
       },
     };
---
+++ data/color-config.ts
@@ -0,0 +1,5 @@
+import { AliasToken } from 'antd/lib/theme/internal';
+
+export const colorConfig: Partial<AliasToken> = {
+  colorPrimary: '#00b96b',
+};
